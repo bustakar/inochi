33c0ea7dde53ada7089556c0e4d0f4bf416a2e59
Karel Busta <karelbusta@gmail.com>
2025-11-26 09:08:02 +0100

    feat: add lint to CI

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -8,15 +8,24 @@ export default function RootLayout() {
   LogBox.ignoreAllLogs();
 
   const [loaded] = useFonts({
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     Bold: require("../src/assets/fonts/Inter-Bold.ttf"),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     SemiBold: require("../src/assets/fonts/Inter-SemiBold.ttf"),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     Medium: require("../src/assets/fonts/Inter-Medium.ttf"),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     Regular: require("../src/assets/fonts/Inter-Regular.ttf"),
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     MBold: require("../src/assets/fonts/Montserrat-Bold.ttf"),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     MSemiBold: require("../src/assets/fonts/Montserrat-SemiBold.ttf"),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     MMedium: require("../src/assets/fonts/Montserrat-Medium.ttf"),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     MRegular: require("../src/assets/fonts/Montserrat-Regular.ttf"),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     MLight: require("../src/assets/fonts/Montserrat-Light.ttf"),
   });
 
