f93c201f831686ee5c72ffbc59db828962fe06f1
Karel Busta <karelbusta@gmail.com>
2025-12-04 15:19:11 +0100

    feat: display stats in profile

--- apps/native/src/app/(tabs)/profile/_layout.tsx
+++ apps/native/src/app/(tabs)/profile/_layout.tsx
@@ -1,17 +1,19 @@
+import { isLiquidGlassAvailable } from "expo-glass-effect";
 import { Stack } from "expo-router";
 
 export default function ProfileLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerLargeTitle: true,
-        headerLargeTitleShadowVisible: false,
-      }}
-    >
+    <Stack>
       <Stack.Screen
         name="index"
         options={{
           title: "Profile",
+          headerStyle: {
+            backgroundColor: isLiquidGlassAvailable()
+              ? "transparent"
+              : "#0D87E1",
+          },
+          headerLargeTitle: true,
         }}
       />
     </Stack>
